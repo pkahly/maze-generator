99f1d824ce239c81249450519b408e111e6d30bc
philip <pkahly@gmail.com>
2020-12-01 16:54:25 -0500

    Fix missing character

--- solvers/BreadthFirstSolver.java
+++ solvers/BreadthFirstSolver.java
@@ -6,6 +6,7 @@ import java.util.List;
 
 public class BreadthFirstSolver extends Solver {
 	List<Point> queue;
+	int moveCount;
 	
 	public BreadthFirstSolver() {
 		queue = new ArrayList<Point>();
@@ -20,15 +21,19 @@ public class BreadthFirstSolver extends Solver {
 	public void setStartLocation(int startRow, int startColumn) {
 		super.setStartLocation(startRow, startColumn);
 		queue.add(new Point(startRow, startColumn));
+		moveCount = 0;
 	}
 	
 	@Override
 	public void nextMove() {
 		List<Point> newQueue = new ArrayList<Point>();
 		
+		// Generate heatmap based on how long it took to reach a spot
+		int visitedColor = VISITED + (moveCount / 200);
+
 		for (Point move : queue) {
-			moveTo(move.x, move.y);
-			setColor(move.x, move.y, VISITED);
+			moveTo(move.x, move.y, visitedColor);
+			setColor(move.x, move.y, visitedColor);
 			for (Point neighbor : getValidMoves()) {
 				int color = getColor(neighbor.x, neighbor.y);
 				if (color == UNVISITED | color == FINISH) { 
@@ -38,10 +43,19 @@ public class BreadthFirstSolver extends Solver {
 		}
 		
 		queue = newQueue;
+		moveCount++;
 	}
 
 	@Override
 	public boolean isDoneTraversing() {
 		return queue.isEmpty();
+	}
+
+	/**
+	* Only stop once the entire maze is explored
+	*/
+	@Override
+	public boolean isFinished() {
+		return isDoneTraversing();
 	}
 }
